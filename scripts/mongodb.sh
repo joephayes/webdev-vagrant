95590fc9a452e9f8a890092884897377161f44a8
Joe Hayes <joephayes@gmail.com>
2016-09-13 00:09:41 -0400

    update to latest mongo version 3.2.x

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -3,13 +3,15 @@
 set -e
 
 # Step 1: Import the MongoDB public key
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 # Step 2: Generate a file with the MongoDB repository url
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 # Step 3: Refresh the local database with the packages
 sudo apt-get -yqq update
 # Step 4: Install the last stable MongoDB version and all the necessary packages on our system
 sudo apt-get install -y mongodb-org
 # Step 5: Modify the mongod.conf file
-sed -i 's/#port = 27017/port = 27017/' /etc/mongod.conf
-sed -i 's/bind_ip = 127.0.0.1/bind_ip = 0.0.0.0/' /etc/mongod.conf
+sudo service mongod stop
+sed -i 's/#port: 27017/port: 27017/' /etc/mongod.conf
+sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/' /etc/mongod.conf
+sudo service mongod start
